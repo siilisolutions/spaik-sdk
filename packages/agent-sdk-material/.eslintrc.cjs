56cbed8356f0e13864d5c742035889bd22e2b846
LeeviSiili <leevi.vahvelainen@siili.com>
2026-01-23 14:30:41 +0200

    Fix/material release pipeline (#36)
    * Add missing ESLint config to agent-sdk-material package
    
    CI was failing because eslint couldn't find a configuration file.
    
    * chore: bump spaik-sdk-material version to 0.1.1
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ packages/agent-sdk-material/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2020: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react', 'react-hooks'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
